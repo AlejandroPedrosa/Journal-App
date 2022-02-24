254e992598340790a9b7003106e4622fea56c4f2
Alejandro Pedrosa <alejandro.pedrosa@hotmail.com>
2022-02-24 19:44:09 -0300

    main journal styles

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const JournalEntry = () => {
+  return (
+    <div className="journal__entry pointer">
+      <div
+        className="journal__entry-picture"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage:
+            "url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/The.Matrix.glmatrix.2.png/800px-The.Matrix.glmatrix.2.png)",
+        }}
+      ></div>
+      <div className="journal__entry-body">
+        <p className="journal__entry-title">Un nuevo dia</p>
+        <p className="journal__entry-content">
+          The standard Lorem Ipsum passage, used since the 1500s
+        </p>
+      </div>
+      <div className="journal__entry-date-box">
+        <span>Monday</span>
+        <h4>28</h4>
+      </div>
+    </div>
+  );
+};
